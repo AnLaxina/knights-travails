373d9d2b06bb7b2a25f244fe8e97a0336f02f009
AnLaxina <anilovlaxina@gmail.com>
2025-07-22 10:18:57 -0700

    Add basic setup for the BFS traversal

--- knight.js
+++ knight.js
@@ -1,7 +1,6 @@
 // Takes paramaters start and end which represent an 8 x 8 chess board start position
 // So start: [0,0] would be the bottom left of the chess board, etc.
 // This function finds the shortest path from start to end based on a knight's movement
-
 function knightMoves(start, end) {
   // Knight can move in 8 directions (an L shape)
   const moves = [
@@ -14,4 +13,30 @@ function knightMoves(start, end) {
     [1, -2],
     [-1, -2],
   ];
+
+  // BFS Setup
+
+  // For the queue, the setup will be [[current position of Knight], [positions visited so far]]
+  const queue = [[start], [[start]]];
+  // Use a set here for fast lookups compared to an array
+  const visited = new Set();
+  console.log(start.toString());
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    const [currentPosition, pathSofar] = queue.shift();
+    const [x, y] = currentPosition;
+
+    // If we have reached the end, stop and return the path
+    if (x === end[0] && y === end[1]) {
+      console.log(
+        `You made it in ${pathSofar.length - 1} moves! Here's your path:`
+      );
+      pathSofar.forEach((position) => console.log(position));
+      return pathSofar;
+    }
+  }
 }
+
+// Testing
+knightMoves([0, 0], [2, 5]);
